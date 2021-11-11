1dd372485ca7424eb897b6efe3e7ff0ba661a509
Ravi Gulia <ravigulia@bitbucket.org>
2021-11-11 13:27:03 +0530

    lamas

--- src/migrations/2023_11_30_999999999_add_index_to_lamas_table.php
+++ src/migrations/2023_11_30_999999999_add_index_to_lamas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class addColumnToLamasAnsTable extends Migration
+class addIndexToLamasTable extends Migration
 {
     /**
      * Run the migrations.
